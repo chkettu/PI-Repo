05507ac0b34d12ca678c60c8e08fa65dd27d2571
Christian Liska <christian.liska@gmail.com>
2022-01-06 07:37:04 +0100

    extract/delete views

--- CheckImage/CheckImage.js
+++ CheckImage/CheckImage.js
@@ -1,13 +1,16 @@
 /**
    Check Image
-   
+
    (c) 2022 by Christian Liska
 */
-#feature-id    CheckImage : Devgroup > CheckImage
+#feature-id Devgroup > CheckImage
 
 #define TITLE "Check Image by CL"
-#define VERSION "1.0.0"
+#define VERSION "1.0.1"
 #define ID "CHKIMG"
+
+#define EXTRACT_VIEWS
+#define DELETE_VIEWS
 
 #include <pjsr/ColorSpace.jsh>
 #include <pjsr/SampleType.jsh>
@@ -23,6 +26,8 @@ function main() {
    var view = window.currentView;
    var img = view.image;
 
+   var viewArray = new Array(9);
+
    width = img.width / 5;
    height = img.height / 5;
 
@@ -36,55 +41,76 @@ function main() {
    var mv = pv.mainView;
    mv.beginProcess();
 
-   let v1 = window.createPreview(0,0,width,height,"L1");
+   let v1 = window.createPreview(0,0,width,height,"_1");
    mv.image.selectedPoint = new Point(0,0);
    mv.image.apply(v1.image);
-   window.deletePreview(v1);
+   viewArray[0] = v1;
 
-   let v2 = window.createPreview(width*2,0,width*2+width,height,"L2");
+   let v2 = window.createPreview(width*2,0,width*2+width,height,"_2");
    mv.image.selectedPoint = new Point(width,0);
    mv.image.apply(v2.image);
-   window.deletePreview(v2);
+   viewArray[1] = v2;
 
-   let v3 = window.createPreview(width*4,0,width*4+width,height,"L3");
+   let v3 = window.createPreview(width*4,0,width*4+width,height,"_3");
    mv.image.selectedPoint = new Point(width*2,0);
    mv.image.apply(v3.image);
-   window.deletePreview(v3);
+   viewArray[2] = v3;
 
-   let v4 = window.createPreview(0,height*2,width,height*2+height,"L4");
+   let v4 = window.createPreview(0,height*2,width,height*2+height,"_4");
    mv.image.selectedPoint = new Point(0,height);
    mv.image.apply(v4.image);
-   window.deletePreview(v4);
+   viewArray[3] = v4;
 
-   let v5 = window.createPreview(width*2,height*2,width*2+width,height*2+height,"L5");
-
+   let v5 = window.createPreview(width*2,height*2,width*2+width,height*2+height,"_5");
    mv.image.selectedPoint = new Point(width,height);
    mv.image.apply(v5.image);
-   window.deletePreview(v5);
+   viewArray[4] = v5;
 
-   let v6 = window.createPreview(width*4,height*2,width*4+width,height*2+height,"L6");
+   let v6 = window.createPreview(width*4,height*2,width*4+width,height*2+height,"_6");
    mv.image.selectedPoint = new Point(width*2,height);
    mv.image.apply(v6.image);
-   window.deletePreview(v6);
+   viewArray[5] = v6;
 
-   let v7 = window.createPreview(0,height*4,width,height*4+height,"L7");
+   let v7 = window.createPreview(0,height*4,width,height*4+height,"_7");
    mv.image.selectedPoint = new Point(0,height*2);
    mv.image.apply(v7.image);
-   window.deletePreview(v7);
+   viewArray[6] = v7;
 
-   let v8 = window.createPreview(width*2,height*4,width*2+width,height*4+height,"L8");
+   let v8 = window.createPreview(width*2,height*4,width*2+width,height*4+height,"_8");
    mv.image.selectedPoint = new Point(width,height*2);
    mv.image.apply(v8.image);
-   window.deletePreview(v8);
+   viewArray[7] = v8;
 
-   let v9 = window.createPreview(width*4,height*4,width*4+width,height*4+height,"L9");
+   let v9 = window.createPreview(width*4,height*4,width*4+width,height*4+height,"_9");
    mv.image.selectedPoint = new Point(width*2,height*2);
    mv.image.apply(v9.image);
-   window.deletePreview(v9);
+   viewArray[8] = v9;
 
    mv.endProcess();
 
    pv.show();
+
+   for (var j = 0; j < viewArray.length; ++j) {
+      var vj = viewArray[j];
+#ifdef EXTRACT_VIEWS
+      var imgj = vj.image;
+      var iwj = new ImageWindow(imgj.width, imgj.height,
+         imgj.numberOfChannels,
+         imgj.bitsPerSample,
+         imgj.sampleType != SampleType_Integer,
+         imgj.colorSpace != ColorSpace_Gray,
+         "C_"+j);
+      var iwjv = iwj.mainView;
+      iwjv.beginProcess();
+      iwjv.image.apply(imgj);
+      iwjv.endProcess();
+      iwj.show();
+#endif
+#ifdef DELETE_VIEWS
+      window.deletePreview(vj);
+#endif
+   }
+
 }
 
 main();
