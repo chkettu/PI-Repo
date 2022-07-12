8faa994be2f23eacfa410a9f3d5651ba9a99e544
Christian Liska <christian.liska@gmail.com>
2022-07-12 13:21:37 +0200

    include pi_lig.jsh

--- CheckImage/Extract3x3crop.js
+++ CheckImage/Extract3x3crop.js
@@ -14,25 +14,6 @@
 
 #include <pjsr/ColorSpace.jsh>
 #include <pjsr/SampleType.jsh>
-
-
-/**
-   Check Image
-
-   (c) 2022 by Christian Liska
-*/
-#feature-id Devgroup > Extract3x3Crop
-
-#define TITLE "Check Image with previews by CL"
-#define VERSION "1.0.0"
-#define ID "CHKPRV"
-
-#define EXTRACT_VIEWS
-#define DELETE_VIEWS
-
-#include <pjsr/ColorSpace.jsh>
-#include <pjsr/SampleType.jsh>
-
 #include "PI_Lib.jsh"
 
 function main() {
